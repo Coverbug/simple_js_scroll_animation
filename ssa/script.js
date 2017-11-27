4b4ad370f5ddc3cb272c90ca778ce2079c7cac09
calamberia <calamberia@gmail.com>
2017-11-27 19:53:05 +0100

    init

---
+++ ssa/script.js
@@ -0,0 +1,54 @@
+class Ssa {
+    constructor() {
+        this.win = window;
+        this.doc = document;
+
+        this.doc.body.setAttribute('data-js', 'on');
+    }
+
+    init() {
+        // get all elements for animation from page
+        this.elements = this.doc.querySelectorAll('[data-scroll-animate="on"]');
+        // get top position of 'this.elements'
+        this.elementsWithTopPosition = this.getElementsWithTopPosition(this.elements);
+
+        // run animation when app starting
+        this.animation(this.elementsWithTopPosition);
+        // run animation when scroll to animated elements
+        this.scrollHandler();
+    }
+
+    animation() {
+        let windowTopPosition = this.doc.documentElement.scrollTop;
+
+        this.elementsWithTopPosition.forEach((item) => {
+            if(windowTopPosition >= item[0]) {
+                setTimeout(() => item[1].removeAttribute('data-scroll-animate-style'), item[2] ? item[2] : 0);
+            }
+        });
+    }
+
+    getElementsWithTopPosition(elemsWithPos) {
+        let rect = 0;
+        let pos = 0;
+        let arr = [];
+        let topOffset = this.win.innerHeight - 50;
+
+        elemsWithPos.forEach((elem) => {
+            rect = elem.getBoundingClientRect();
+            pos = rect.top - topOffset;
+
+            // get top position
+            arr.push([[pos], elem, elem.getAttribute('data-scroll-animate-delay')]);
+        });
+
+        return arr;
+    }
+
+    scrollHandler() {
+        this.win.addEventListener('scroll', () => this.animation());
+    }
+}
+
+let instance = new Ssa();
+instance.init();
